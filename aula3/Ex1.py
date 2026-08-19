b500374b9c026a8e91ea4680c5fbb3b14e9095bf
Pedro Henrique dos Santos Araujo <phsa2004@gmail.com>
2026-08-19 18:24:56 -0300

    add

---
+++ aula3/Ex1.py
@@ -0,0 +1,36 @@
+classificacao = ('Flamengo','Barcelona','Real Madrid','Pouso Alegre','Remo')
+print(f'3 primeiros colocados: {classificacao[:3]}')
+print(f'Ultimos 2 colocados: {classificacao[3:]}')
+print(f'Times em ordem alfabetica: {sorted(classificacao)}')
+print(f'Posição do Barcelona: {classificacao.index("Barcelona")}')
+
+
+print(' ')
+
+
+loja1 = {'Iphone 15', 'Galaxy S24', 'Xiaomi 14', 'Moto G84'}
+loja2 = {'Galaxy S24', 'Xiaomi 14', 'Pixel 8', 'Iphone 14'}
+total_modelos = loja1 | loja2
+print(f'Modelos no total (opção de comprar visitando as duas): {total_modelos}')
+modelos_ambas = loja1 & loja2
+print(f'Modelos disponíveis em ambas as lojas: {modelos_ambas}')
+
+
+print(' ')
+
+
+nome = input('Digite o nome do aluno: ')
+media = float(input('Digite a média do aluno: '))
+
+if media >= 50:
+    situacao = 'AP'
+else:
+    situacao = 'RP'
+
+aluno = {
+    'nome': nome,
+    'media': media,
+    'situacao': situacao
+}
+
+print(aluno)
